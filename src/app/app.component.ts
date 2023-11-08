4aeb6f886949ea257538b898a40ccc9b97b91f4c
Staicu Alexandru-Robert <rstaicu@pentalog.com>
2023-11-08 20:01:32 +0200

    MAP-000: solve build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
 })
 export class AppComponent {
-  title = 'map';
+  title = 'map123';
 }
